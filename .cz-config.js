1840ec079aa0b9f083f60c2d1c5320a270678386
hank <steven-online@hotmail.com>
2020-10-28 17:52:59 +0800

    fix: add react-hooks eslint

--- .cz-config.js
+++ .cz-config.js
@@ -6,26 +6,26 @@ module.exports = {
     {
       value: 'style',
       name:
-        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'
     },
     {
       value: 'refactor',
-      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature'
     },
     {
       value: 'perf',
-      name: 'perf:     A code change that improves performance',
+      name: 'perf:     A code change that improves performance'
     },
     { value: 'test', name: 'test:     Adding missing tests' },
     {
       value: 'chore',
       name:
-        'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+        'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'
     },
     { value: 'revert', name: 'revert:   Revert to a commit' },
-    { value: 'WIP', name: 'WIP:      Work in progress' },
+    { value: 'WIP', name: 'WIP:      Work in progress' }
   ],
-
+  // 模块
   scopes: [{ name: 'hooks-component' }, { name: 'redux' }, { name: 'component' }, { name: 'test' }],
 
   allowTicketNumber: false,
@@ -47,14 +47,14 @@ module.exports = {
   // override the messages, defaults are as follows
   messages: {
     type: "Select the type of change that you're committing:",
-    scope: '\nDenote the SCOPE of this change (optional):',
+    scope: '\nDenote the SCOPE of this change (optional):', // 范围
     // used if allowCustomScopes is true
     customScope: 'Denote the SCOPE of this change:',
     subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
     body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
     breaking: 'List any BREAKING CHANGES (optional):\n',
     footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?'
   },
 
   allowCustomScopes: true,
@@ -63,5 +63,5 @@ module.exports = {
   skipQuestions: ['body'],
 
   // limit subject length
-  subjectLimit: 100,
+  subjectLimit: 100
 }
--- README.md
+++ README.md
@@ -17,6 +17,9 @@ You will also see any lint errors in the console.
 Launches the test runner in the interactive watch mode.<br>
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
+### node version
+ >=10.22.0
+
 ### `npm run build`
 
 Builds the app for production to the `build` folder.<br>
